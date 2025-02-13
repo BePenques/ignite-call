45aad11da0d667e2de360af4291bcf88f55cc53a
Betiza Barreira <be.penques@hotmail.com>
2025-02-13 19:32:39 -0300

    initial config

---
+++ src/components/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { globalStyles } from "@/styles/global";
+
+ // Agora esse arquivo é um Client Component
+
+
+
+export function GlobalStyles() {
+  globalStyles() // Aplica os estilos globais
+  return null // Esse componente não renderiza nada, só aplica os estilos
+}
