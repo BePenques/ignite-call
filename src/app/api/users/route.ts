30d3707cf1bb6ce0f6705e1bd5c2cb054a1b51b1
Betiza Barreira <be.penques@hotmail.com>
2025-04-23 09:06:59 -0300

    manipulando cookies no next

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
+import { setCookie } from 'nookies';
 
 interface UserData {
   name: string;
@@ -14,14 +15,30 @@ export async function POST(request: NextRequest) {
    
     const { name, username } = body as UserData;
 
+    const userExist = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    })
+
+    if(userExist){
+      return NextResponse.json({ error: 'Username already exists' }, { status: 400 });
+    }
+
     const user = await prisma.user.create({
       data:{
         name,
         username
       }
     })
+    const response = NextResponse.json(user, { status: 201 });
 
-    return NextResponse.json(user, { status: 201 });
+    response.cookies.set('@ignitecall:userId', user.id, {
+      maxAge: 60 * 60 * 24 * 7, // 7 dias
+      path: '/',
+    });
+
+    return response;
   } catch (error) {
     return NextResponse.json({ error: 'Erro ao registrar usuário' }, { status: 500 });
   }
